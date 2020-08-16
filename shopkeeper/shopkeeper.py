15b05b03662bac6bb3852d65ca7008c75796379e
noah-green <ngreen612@gmail.com>
2020-08-16 19:48:01 -0400

    incremental commit

--- shopkeeper/shopkeeper.py
+++ shopkeeper/shopkeeper.py
@@ -1,3 +1,4 @@
+import os
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
@@ -5,3 +6,5 @@ bot = commands.Bot(command_prefix='!')
 @bot.event
 async def on_ready():
     print(f'logged in as {bot.user}')
+
+bot.run(os.environ['DISCORD_TOKEN'])
